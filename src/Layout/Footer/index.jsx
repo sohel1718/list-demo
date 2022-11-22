ff869d3d9b39c7a08987b12e1c3d9ec38acfdfa6
sohel chhipa <sohel.chhipa@artoon.in>
2022-11-22 11:02:22 +0530

    dynamic var

--- src/Layout/Footer/index.jsx
+++ src/Layout/Footer/index.jsx
@@ -3,53 +3,6 @@ import { ReactComponent as Facebook } from "../../assets/images/facebook.svg";
 import { ReactComponent as Twitter } from "../../assets/images/twitter.svg";
 
 const Footer = () => {
-<<<<<<< HEAD
-    return (
-        <div className="footer">
-            <div className="footer_address">
-                <div className="footer_address_logo">
-                    <div>Logo</div>
-                </div>
-                <div className="footer_address_text">
-                    1901 Thornridge Cir. Shiloh, Hawaii 81063
-                </div>
-            </div>
-            <div className="footer_nav">
-                <div className="footer_nav_block">
-                    <h1>Company</h1>
-                    <span>About</span>
-                    <span>Career</span>
-                    <span>Team</span>
-                    <span>Contact</span>
-                </div>
-                <div className="footer_nav_block">
-                    <h1>Product</h1>
-                    <span>Service</span>
-                    <span>Feelancer</span>
-                    <span>Features</span>
-                    <span>Development</span>
-                </div>
-                <div className="footer_nav_block">
-                    <h1>Links</h1>
-                    <span>Privacy Policy</span>
-                    <span>Term & Conditions</span>
-                    <span>FAQ</span>
-                </div>
-            </div>
-            <div className="footer_social">
-                <h1>Follow Us</h1>
-                <div className="footer_social_icons">
-                    <Twitter />
-                    <Facebook />
-                    <Instagram />
-                </div>
-            </div>
-        </div>
-    )
-}
-
-export default Footer
-=======
   return (
     <div className="footer">
       <div className="footer_address">
@@ -93,4 +46,3 @@ export default Footer
 };
 
 export default Footer;
->>>>>>> 4e98f6432ce6c78ca3ff4dde88fc9e22da9ac98c
