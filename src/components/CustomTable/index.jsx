a2ba6a58b6e5e7d8bdb2781ef1b56b28b8c150b0
sohel.chhipa <sohel.chhipa@artoon.in>
2022-11-23 01:05:56 +0530

    updated design

--- src/components/CustomTable/index.jsx
+++ src/components/CustomTable/index.jsx
@@ -1,9 +1,8 @@
+import Pagination from './Pagination';
 import { useMemo, useState } from 'react';
 import Filter from './Filter';
-import Pagination from './Pagination';
-// import { ReactComponent as DownArrow } from '../../assets/images/down_arrow.svg';
 
-const CustomTable = ({ dataSource, columns, className = '', PageSize = 2 }) => {
+const CustomTable = ({ dataSource, columns, className = '', PageSize=2 }) => {
   const [currentPage, setCurrentPage] = useState(1);
 
   const currentTableData = useMemo(() => {
@@ -13,69 +12,53 @@ const CustomTable = ({ dataSource, columns, className = '', PageSize = 2 }) => {
   }, [currentPage, dataSource]);
 
   const headerRender = (data) => {
-    return (
-      <div className={`custom-table_content_header_col`}>
-        {data.title}
-      </div>
-    );
+    return <th style={{ textAlign: data.align }} >{data.title}</th>;
   };
 
   const bodyRender = (data) => {
     return (
-      <div className='custom-table_content_row'>
+      <>
+        <tr>
         {columns.map((col) => {
           switch (col.dataIndex) {
             case 'action':
-              return (
-                <div
-                  className={`custom-table_content_row_col`}
-                >
-                  {col.render()}
-                </div>
-              );
+              return <td style={{ textAlign: col.align }}>{col.render()}</td>;
             default:
               if (col.render) {
-                return (
-                  <div
-                    className={`custom-table_content_row_col`}
-                  >
-                    {col.render(data)}
-                  </div>
-                );
+                return <td style={{ textAlign: col.align }} >{col.render(data)}</td>;
               }
-              return (
-                <div
-                  className={`custom-table_content_row_col`}
-                >
-                  {data[col.dataIndex]}
-                </div>
-              );
+              return <td style={{ textAlign: col.align }} >{data[col.dataIndex]}</td>;
           }
         })}
-      </div>
+      </tr>
+      <hr />
+      </>
     );
   };
 
   return (
     <div className='custom-table'>
       <Filter />
-      <div className={`custom-table_content ${className}`}>
-        <div className='custom-table_content_header'>
-          {columns.map((data) => {
-            return headerRender(data);
+      <table className={className}>
+        <thead>
+          <tr>
+            {columns.map((data) => {
+              return headerRender(data);
+            })}
+          </tr>
+        </thead>
+        <tbody>
+          {currentTableData.map((data) => {
+            return bodyRender(data);
           })}
-        </div>
-        {currentTableData.map((data) => {
-          return bodyRender(data);
-        })}
-      </div>
-
-      <Pagination
-        className='pagination-bar'
+        </tbody>
+      </table>
+      <Pagination 
+        className="pagination-bar"
         currentPage={currentPage}
         totalCount={dataSource.length}
         pageSize={PageSize}
-        onPageChange={(page) => setCurrentPage(page)}
+        onPageChange={page => setCurrentPage(page)}
       />
     </div>
   );
--- src/pages/projects/Projects.jsx
+++ src/pages/projects/Projects.jsx
@@ -12,27 +12,26 @@ const Projects = () => {
 
   useEffect(() => {
     setData(getData());
-  }, []);  
-  
+  }, []);
+
   const columns = [
     {
       title: "Buyer",
       dataIndex: "buyer",
       key: "buyer",
-      className: "text_center jc_left",
+      align: "center",
       render: (data) => {
         let color = stringToHslColor(data.buyer);
         let iconColor = stringToHslColor(data.buyer, 50, 40);
         return (
-          <div className="buyer">
-            <div className="buyer_left">
-            <div style={{ backgroundColor: color }} className="buyer_left_avatar">
+          <div className="buyer_left">
+            <div
+              style={{ backgroundColor: color }}
+              className="buyer_left_avatar"
+            >
               <User style={{ fill: iconColor }} />
             </div>
-            <div className="buyer_left_title">
-              {data.buyer}
-            </div>
-            </div>
+            <div className="buyer_left_title">{data.buyer}</div>
           </div>
         );
       },
@@ -41,17 +40,17 @@ const Projects = () => {
       title: "Project Details",
       dataIndex: "projectDetails",
       key: "projectDetails",
-      className: "text_left jc_left",
+      align: "left",
       //width: '194px',
       render: (data) => {
-        return <p className="details_text text_ellipse" >{data.projectDetails}</p>;
+        return <p className="text_ellipse" >{data.projectDetails}</p>;
       },
     },
     {
       title: "Price",
       dataIndex: "price",
       key: "price",
-      className: "text_center jc_center",
+      align: "center",
       //width: '194px',
       render: (data) => {
         return (
@@ -66,7 +65,7 @@ const Projects = () => {
       title: "Delivery Time",
       dataIndex: "deliveryTime",
       key: "deliveryTime",
-      className: "text_center jc_center",
+      align: "center",
       //width: '194px',
       render: (data) => {
         let color = stringToHslColor(data.buyer);
@@ -85,7 +84,7 @@ const Projects = () => {
       title: "Post Day",
       dataIndex: "postDay",
       key: "postDay",
-      className: "text_center jc_center",
+      align: "center",
       //width: '194px',
       render: (data) => {
         return (
@@ -97,7 +96,7 @@ const Projects = () => {
       title: "Offer",
       dataIndex: "offer",
       key: "offer",
-      aclassName: "text_center jc_center",
+      align: "center",
       //width: '194px',
       render: (data) => {
         return <span className="fw_600">{data.offer}</span>;
@@ -122,17 +121,25 @@ const Projects = () => {
   const searchData = (e) => {
     const { value } = e.target;
     setData(searchBuyer(value));
-  }
+  };
 
   return (
     <div className="dash">
       <div className="dash_search">
         <div className="dash_search_search-input">
-          <input onChange={searchData} type="text" placeholder="Search more projects" />
+          <input
+            onChange={searchData}
+            type="text"
+            placeholder="Search more projects"
+          />
           <Search />
         </div>
         <div className="dash_search_res-search">
-          <input onChange={searchData} type="text" placeholder="Search more projects" />
+          <input
+            onChange={searchData}
+            type="text"
+            placeholder="Search more projects"
+          />
           <button>
             <Search />
           </button>
